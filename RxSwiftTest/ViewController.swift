0696a49c7dd14b307355a72c2ba739fda15b08fb
Tomoko Ikenoue <tomoko@tomokonoMacBook-puro.local>
2019-04-07 18:10:26 +0900

    新規作成

---
+++ RxSwiftTest/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  RxSwiftTest
+//
+//  Created by Tomoko Ikenoue on 2019/02/17.
+//  Copyright © 2019 Tomoko Ikenoue. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    let disposeBag = DisposeBag()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        textField.rx.text.orEmpty
+            .map {$0.description}
+            .bind(to: label.rx.text)
+            .disposed(by: disposeBag)
+    }
+
+
+}
+
